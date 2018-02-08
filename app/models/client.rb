957e0797957efbed4931e8bc323f8efe3c0dd2c6
sangpt <anhsang222@gmail.com>
2018-02-08 18:29:53 +0700

    recreate project

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,11 +1,30 @@
 class Client
-  include ActiveModel::SecurePassword
   include Mongoid::Document
   include Mongoid::Timestamps
-  
-  has_secure_password
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   field :username, type: String
+  field :email, type: String
   field :password_digest, type: String
+  field :email, type: String, default: ""
+  field :encrypted_password, type: String, default: ""
+
+  ## Recoverable
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
+
+  ## Rememberable
+  field :remember_created_at, type: Time
+
+  ## Trackable
+  field :sign_in_count,      type: Integer, default: 0
+  field :current_sign_in_at, type: Time
+  field :last_sign_in_at,    type: Time
+  field :current_sign_in_ip, type: String
+  field :last_sign_in_ip,    type: String
+
   has_and_belongs_to_many :services
   has_many :requests
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,11 +1,9 @@
 class Service
   include Mongoid::Document
   include Mongoid::Timestamps
-  
+
   field :type, type: String
   field :name, type: String
-  field :subservices, type: String
-  field :clients, type: String
-  embeds_many :subservices
+  has_many :subservices
   has_and_belongs_to_many :clients
 end
